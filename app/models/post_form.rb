924651a2a547a02bd760cc1ad6b3ed4a540302af
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-11-30 19:47:25 +0900

    formオブジェクト　編集機能

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -2,14 +2,17 @@ class PostForm
   include ActiveModel::Model
 
   #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
-  attr_accessor :text, :image
+  attr_accessor(
+    :text, :image,
+    :id, :created_at, :updated_at
+  )
 
   with_options presence: true do
     validates :text
     validates :image
   end
 
-  def save
-    Post.create(text: text, image: image)
+  def update(params, post)
+    post.update(params)
   end
 end
